ed0afe183bc6cc973279914da5ea55611e927f31
Squidoinkoo <rwoodkat4@gmail.com>
2018-10-28 16:07:24 -0400

    Read this plz

--- src/warframes/ReadMe.java
+++ src/warframes/ReadMe.java
@@ -1,10 +1,11 @@
 package warframes;
 
-/*
-The point of all of this is to write (type?) down ideas/suggestions for Warframe abilities and passives that should get a rework.
+public class ReadMe{
 
-The point of this is NOT to make fun of, roast, or satirise Warframe content. Digital Extremes has put a large amount of effort into
-their content.
+/*
+Tell me whether or not ideas for weapon reworks would be a good idea, of if weapons are fine the way they are.
 
 -Squidoinkoo
 */
+	
+}
