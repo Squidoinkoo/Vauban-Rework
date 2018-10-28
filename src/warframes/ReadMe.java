cf0e0017143f89a93c100a10a512b744769e4b27
Squidoinkoo <rwoodkat4@gmail.com>
2018-10-28 16:09:45 -0400

    Read this

--- src/warframes/ReadMe.java
+++ src/warframes/ReadMe.java
@@ -1,11 +1,3 @@
-package warframes;
-
-public class ReadMe{
-
-/*
 Tell me whether or not ideas for weapon reworks would be a good idea, of if weapons are fine the way they are.
 
 -Squidoinkoo
-*/
-	
-}
