320e09a87e074aa59125a9a088656e564b86ee2c
Squidoinkoo <rwoodkat4@gmail.com>
2018-10-28 02:08:50 -0400

    Read this

---
+++ src/Warframe/ReadMe.java
@@ -0,0 +1,8 @@
+/* The purpose of this is not to make fun of any Warframes or their abilities, but just to discuss some potential things that should be reworked.
+
+I don't expect this to be too terribly public; if you're reading this, you're probably the only one.
+
+Give me some feedback on what you think of my ideas.
+
+My email is rwoodkat4@gmail.com
+*/
