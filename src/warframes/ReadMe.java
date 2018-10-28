c75009bccccf3ab0488eb4a3668cc91ee4e3700e
Squidoinkoo <rwoodkat4@gmail.com>
2018-10-28 15:52:23 -0400

    Read this

---
+++ src/warframes/ReadMe.java
@@ -0,0 +1,10 @@
+package warframes;
+
+/*
+The point of all of this is to write (type?) down ideas/suggestions for Warframe abilities and passives that should get a rework.
+
+The point of this is NOT to make fun of, roast, or satirise Warframe content. Digital Extremes has put a large amount of effort into
+their content.
+
+-Squidoinkoo
+*/
